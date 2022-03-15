df7c02b6fd26b06c27f64284c2ae67421bc541ff
mecon2000 <mecon2000@gmail.com>
2022-03-16 00:16:13 +0200

    adding actual branches,leaves,and sub-leaves

--- src/DrawingBranch.js
+++ src/DrawingBranch.js
@@ -1,26 +1,107 @@
-import React from "react";
+//import React from "react";
 import Sketch from "react-p5";
+import { Leaf } from "./Leaf.js";
 
-let x = 50;
-let y = 50;
-const DrawingBranch =  (props) => {
+//consts:
+let leaves = [];
+const canvasSize = 800;
+const maximumLeaves = 10;
+
+//colors consts:
+let backgroundColor;
+let branchColor;
+const minBranchWidth = 1;
+const maxBranchWidth = 7;
+
+//globals :
+let currentMinimumDistance = 0;
+
+const DrawingBranch = (props) => {
   const setup = (p5, canvasParentRef) => {
+    window.p5instance = p5; //TODO there must be a better way to get the only p5 instance
+    //setting consts which cannot be initialized on global scope (it's a p5.js thing)
+    backgroundColor = p5.color("#F5C67B");
+    branchColor = p5.color("white");
+
     // use parent to render the canvas in this ref
     // (without that p5 will render the canvas outside of your component)
-    p5.createCanvas(500, 500).parent(canvasParentRef);
+    p5.createCanvas(canvasSize, canvasSize).parent(canvasParentRef);
+    p5.frameRate(30);
+    p5.rectMode(p5.CENTER);
+    p5.stroke(branchColor);
+    p5.background(backgroundColor);
+    p5.textSize(canvasSize / 30);
+    p5.textAlign(p5.CENTER);
   };
 
   const draw = (p5) => {
-    p5.background(0);
-    p5.ellipse(x, y, 70, 70);
     // NOTE: Do not use setState in the draw function or in functions that are executed
     // in the draw function...
     // please use normal variables or class properties for these purposes
-    x++;
+    writeInstructions(p5);
+
+    leaves.forEach((l) => {
+      l?.leaf?.draw();
+    });
   };
 
-  return <Sketch setup={setup} draw={draw} />;
+  const writeInstructions = (p5) => {
+    p5.push();
+    p5.stroke("white");
+    p5.strokeWeight(1);
+    p5.fill("black");
+    p5.text("drag the mouse over the screen", canvasSize / 2, 35);
+    p5.pop();
+  };
+
+  const setBranchWidth = (p5) => {
+    const weight = p5.lerp(
+      maxBranchWidth,
+      minBranchWidth,
+      leaves.length / maximumLeaves
+    );
+    p5.strokeWeight(weight);
+  };
+
+  const getLineAngle = (p5, v1, v2) => {
+    const v3 = p5.createVector(v2.x - v1.x, v2.y - v1.y);
+    const angle = v3.heading();
+    return angle;
+  };
+  const mousePressed = (p5) => {
+    leaves = [];
+    p5.background(backgroundColor);
+  };
+
+  const mouseDragged = (p5) => {
+    const currLoc = p5.createVector(p5.mouseX, p5.mouseY);
+    if (leaves.length !== 0) {
+      const prevLeaf = leaves[leaves.length - 1];
+      if (
+        leaves.length < maximumLeaves &&
+        currLoc.dist(prevLeaf.loc) > currentMinimumDistance
+      ) {
+        setBranchWidth(p5);
+        p5.line(prevLeaf.loc.x, prevLeaf.loc.y, currLoc.x, currLoc.y);
+        const angle = getLineAngle(p5, prevLeaf.loc, currLoc);
+        leaves.push({ loc: currLoc, leaf: new Leaf(currLoc, angle) });
+        currentMinimumDistance = p5.random(30, 120);
+      }
+    } else {
+      //first time:
+      leaves.push({ loc: currLoc });
+      currentMinimumDistance = p5.random(40, 120);
+    }
+  };
+
+  return (
+    <Sketch
+      setup={setup}
+      draw={draw}
+      mouseDragged={mouseDragged}
+      mousePressed={mousePressed}
+    />
+  );
 };
 
-
-export {DrawingBranch}
+export { DrawingBranch };
---
+++ src/Leaf.js
@@ -0,0 +1,105 @@
+const initialStroke = 5;
+const maxSteps = 50;
+class Leaf {
+  constructor(loc, branchAngle) {    
+    const p5 = window.p5instance
+    this.width = 35;
+    this.height = 55;
+    this.setToInitial(loc, branchAngle);
+    this.color = { start: p5.color("PaleGreen"), end: p5.color("PaleGoldenrod") };
+    this.outlineColor = { start: p5.color("Chartreuse"), end: p5.color("DarkGreen") };
+    this.stemColor = p5.color("White");
+    this.name = "master";
+  }
+
+  setToInitial(location = window.p5instance.createVector(0, 0), branchAngle) {  
+    const p5 = window.p5instance
+    this.subLeaves = [];
+    this.subLeaves.push(new SubLeaf("son1", p5.createVector(38, p5.random(-7, 7))));
+    this.subLeaves.push(new SubLeaf("son2", p5.createVector(-38, p5.random(-7, 7))));
+    this.subLeaves.push(new SubLeaf("son3", p5.createVector(p5.random(-7, 7), -50)));
+
+    this.currentStep = maxSteps;
+    const side = p5.random([60, -60]);
+    this.stemAngle = p5.radians(p5.degrees(branchAngle) + side + p5.random(-45, +45));
+    this.leafAngle = p5.radians(p5.random(80, 110));
+    this.stemStart = location;
+    
+    let v = window.p5.Vector.fromAngle(this.stemAngle, 50);
+    this.stemEnd = window.p5.Vector.add(this.stemStart, v);
+  }
+
+  draw() {
+    const p5 = window.p5instance
+    if (this.currentStep > 0) {
+      this.fadeLeaf();
+    } else {
+      return;
+    }
+
+    p5.strokeWeight(2);
+    p5.stroke(this.stemColor);
+    p5.line(this.stemStart.x, this.stemStart.y, this.stemEnd.x, this.stemEnd.y);
+
+    p5.push();
+    p5.translate(this.stemEnd);
+    p5.rotate(this.stemAngle + this.leafAngle);
+    p5.rect(0, 0, this.width, this.height, 15, 15, 5, 5);
+    this.subLeaves.forEach((l) => l.draw());
+    p5.pop();
+  }
+
+  fadeLeaf() {
+    const p5 = window.p5instance
+    this.currentStep--;
+    let s = initialStroke * (this.currentStep / maxSteps);
+    p5.strokeWeight(s);
+
+    let leafOutlineColor = p5.lerpColor(
+      this.outlineColor.start,
+      this.outlineColor.end,
+      1 - this.currentStep / maxSteps
+    );
+    p5.stroke(leafOutlineColor);
+
+    let leafColor = p5.lerpColor(
+      this.color.start,
+      this.color.end,
+      1 - this.currentStep / maxSteps
+    );
+    p5.fill(leafColor);
+  }
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class SubLeaf extends Leaf {
+  constructor(name, location) {
+    super();
+    this.width = 10;
+    this.height = 10;
+    this.name = name;
+    this.setToInitial(location);
+  }
+
+  setToInitial(location = window.p5instance.createVector(0, 0)) {
+    const p5 = window.p5instance
+    this.location = location;
+    this.currentStep = maxSteps;
+    this.color = { start: p5.color("IndianRed"), end: p5.color("DarkRed") };
+    this.outlineColor = { start: p5.color("Chartreuse"), end: p5.color("DarkGreen") };
+  }
+
+  draw() {
+    if (this.currentStep > 0) {
+      this.fadeLeaf();
+    }
+
+    const p5 = window.p5instance
+    p5.push();
+    p5.translate(this.location);
+    p5.rect(0, 0, this.width, this.height, 3, 3, 3, 3);
+    console.log(this.name);
+    p5.pop();
+  }
+}
+
+export { Leaf };
