626b6effde978692d9f26309615ddcdb03a91a34
mecon2000 <mecon2000@gmail.com>
2022-03-18 01:28:18 +0200

    Added sliders (which do nothing for now), with flexbox for responsiveness (sliders are ugly, for now)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import { DrawingBranch } from "./Components/DrawingBranch.js";
 import { Footer } from "./Layouts/Footer.js";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 import "./App.css";
+import { Fragment } from "react";
+
+const sliderChange = (v) => {
+  console.log("slider changed to ", v);
+};
 
 function App() {
   return (
-    <div className="App">
+    <Fragment>
       <header className="header" />
-      <DrawingBranch className="drawing-branch" />
+      <div className="main">
+        <DrawingBranch className="drawing-branch" />
+        <div className="sliders-container">
+          <Slider
+            className="slider"
+            min={1}
+            max={10}
+            step={1}
+            onChange={sliderChange}
+            defaultValue={5}
+            vertical={true}
+          />
+          <Slider className="slider" vertical={true}/>
+          <Slider className="slider" vertical={true}/>
+          <Slider className="slider" vertical={true}/>
+          <Slider className="slider" vertical={true}/>
+          <Slider className="slider" vertical={true}/>
+        </div>
+      </div>
       <Footer />
-    </div>
+    </Fragment>
   );
 }
 
