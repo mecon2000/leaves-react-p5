f1840d9a70dbe05271f013d29207bcc3f6b4bf14
mecon2000 <mecon2000@gmail.com>
2022-03-19 01:12:56 +0200

    Footer content in the middle

--- src/Layouts/Footer.js
+++ src/Layouts/Footer.js
@@ -6,13 +6,15 @@ function Footer() {
   return (
     <footer className="Footer">
       <hr />
-      Rong
-      <a href="https://github.com/mecon2000">
-        <img src={github} alt="github"></img>
-      </a>
-      <a href="https://www.linkedin.com/in/ronnie-ganot-9065b613/">
-        <img src={linkedInLogo} alt="LinkedIn"></img>
-      </a>
+      <div className="footer-content">
+        Rong
+        <a href="https://github.com/mecon2000">
+          <img src={github} alt="github"></img>
+        </a>
+        <a href="https://www.linkedin.com/in/ronnie-ganot-9065b613/">
+          <img src={linkedInLogo} alt="LinkedIn"></img>
+        </a>
+      </div>
     </footer>
   );
 }
