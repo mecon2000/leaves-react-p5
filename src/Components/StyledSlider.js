282979b0b06d614e2c22af3bd6a837b080b9e59f
mecon2000 <mecon2000@gmail.com>
2022-03-21 00:51:36 +0200

    added value to the slider (instead of tooltip of the value)

--- src/Components/StyledSlider.js
+++ src/Components/StyledSlider.js
@@ -1,13 +1,23 @@
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
+import { useState } from "react";
 import "./StyledSlider.css";
 
 function StyledSlider(props) {
-  let { title, ...otherProps } = props;
+  let { title, onChange, ...otherProps } = props;
+  let [sliderValue,setSliderValue] = useState(otherProps.defaultValue);
+  
+  const handleChange = (v) => {
+    setSliderValue(v);
+    onChange(v);
+  }
+
+
   return (
     <div className="slider-wrapper">
       <div className="title">{title}</div>
-      <Slider {...otherProps} className="slider" vertical={true} />
+      <Slider onChange={handleChange} {...otherProps} className="slider" vertical={true} />
+      { sliderValue }
     </div>
   );
 }
